66a17f8ec28acf7a7767bd325c54936102609065
Wagner, J <35028309+dota247@users.noreply.github.com>
2020-01-23 12:53:13 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 yum update -y
-yum install bind -y
+yum -y install bind bind-utils
+
+#By defaulf, BIND listens on localhost. So, we will configure DNS server to listen on system IP address 
+#to let clients can reach to DNS server for resolving domain names.
+
+vi /etc/named.conf
